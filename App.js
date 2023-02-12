f106b8cecd361ce8ad3736a432a583e1b7a628d7
Diego Vester <diego.vester@outlook.com>
2023-02-11 20:32:25 -0600

    Navigate to a route multiple times

--- App.js
+++ App.js
@@ -17,10 +17,14 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
     </View>
   );
 }
